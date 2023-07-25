df8709553eff2ed9860de2bce34ac042c39ecac0
Adionaps <125360073+Adionaps@users.noreply.github.com>
2023-07-25 19:02:14 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,8 @@
+Player1_name=localStorage.getItem("player1_name")
+Player2_name=localStorage.getItem("player2_name")
+Player1_score=0
+Player2_score=0
+document.getElementById("player1_name").innerHTML=Player1_name+" : "
+document.getElementById("player2_name").innerHTML=Player2_name+" : "
+document.getElementById("player1_score").innerHTML=Player1_score
+document.getElementById("player2_score").innerHTML=Player2_score
